23b219aa041e9ced8542f8690b45a2c9f5f0d4f1
kjh <rlawngud1999@gmail.com>
2026-04-06 11:27:38 +0900

    push 테스트

---
+++ src/main/java/com/example/jenkins/TestController.java
@@ -0,0 +1,11 @@
+package com.example.jenkins;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    public String test(){
+        return "zzz";
+    }
+}
