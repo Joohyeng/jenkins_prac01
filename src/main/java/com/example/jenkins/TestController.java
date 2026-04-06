97a0f0ef1c0b2068f13b34c3bb4a119b5204ef90
kjh <rlawngud1999@gmail.com>
2026-04-06 14:23:05 +0900

    test

--- src/main/java/com/example/jenkins/TestController.java
+++ src/main/java/com/example/jenkins/TestController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     public String test(){
-        return "zzz4";
+        return "zzz5";
     }
 }
