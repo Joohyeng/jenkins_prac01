ba2dec178ac809560703f573225f9e073b2ae2b8
kjh <rlawngud1999@gmail.com>
2026-04-06 17:53:58 +0900

    test

--- src/main/java/com/example/jenkins/TestController.java
+++ src/main/java/com/example/jenkins/TestController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/abc")
     public String test(){
-        return "v2";
+        return "v1";
     }
 }
